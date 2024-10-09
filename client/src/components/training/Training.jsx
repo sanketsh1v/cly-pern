2f2635cd8da7000b8230d9db8aed364c6771a5a8
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-10-09 07:47:25 +0545

    Update Training.jsx

--- client/src/components/training/Training.jsx
+++ client/src/components/training/Training.jsx
@@ -11,29 +11,26 @@ const Training = () => {
         <img src="/training.jpg" alt="Training" className="training__image" />
 
         <h3 className="training__title">Certified Laughter Yoga Leader Training</h3>
-        
+
         <p className="training__description">
-          Enrich your life with laughter! Join us for this unique weekend training program that may change the way you live with joy through laughter! After the completion of the training, you will be qualified as a Certified Laughter Yoga Leader (CLYL) with the Laughter Yoga International University; this is an internationally recognized certification. As CLYL, you can lead Laughter Yoga sessions with corporate clients, seniors and healthcare facilities, schools, colleges and universities, yoga and fitness centres, community groups and not-for-profit organizations, or integrate Laughter Yoga into your own business practice, instruction, or healing modality.
+          Embark on a life-changing journey that will bring laughter into your life. During the training, you’ll dive into Laughter Yoga and the power of joyful practice.
         </p>
 
-        {/* Box for Schedule, Fee, and Register Button */}
+        {/* Box for Date, Price, and Register Button */}
         <div className="training__details-box">
-          <div className="training__schedule">
-            <p>October 25-27, 2024 | Friday 6:30-8:30pm | Saturday 9:00-5:00pm | Sunday 9:00am-3:00pm</p>
-            <p>9:00am-3:00pm with a public event at 1:00pm</p>
-          </div>
-          
-          <div className="training__fee">
-            <p>$395 early bird (by Sept 25); $495 regular price</p>
-          </div>
-
+          <p className="training__schedule">
+            October 25-27, 2024 | Friday 6:30-8:30pm | Saturday 9:00-5:00pm | Sunday 9:00am-3:00pm
+          </p>
+          <p className="training__fee">
+            Fee: $395 early bird by Sept 25; $495 regular price
+          </p>
           <button className="training__button">Register</button>
         </div>
 
-        {/* Separate Box for the Inquire Message */}
+        {/* Separate Box for "Please Inquire" Message */}
         <div className="training__inquire-box">
           <p className="training__note">
-            <strong>Partial scholarships may be available if you are facing financial barriers – please inquire.</strong>
+            Partial scholarships may be available if you are facing financial barriers – please inquire.
           </p>
         </div>
       </div>
