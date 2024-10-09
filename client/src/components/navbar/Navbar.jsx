0c27e1c6cf72a70fcc5547e2a7e2f4bf4528df11
Anamika Rijal <anamika.rijal.13@gmail.com>
2024-10-09 07:47:36 +0545

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.scss';
 
-
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">
-        <img src="./logo.png" alt="Logo" className="navbar__logo-image" />
+      <div className="navbar__content">
+        <div className="navbar__logo-container">
+          <img src="/logo.png" alt="Logo" className="navbar__logo-image" />
+        </div>
+        <div className="navbar__links">
+          <Link to="/" className="navbar__link">Home</Link>
+          <Link to="/events" className="navbar__link">Upcoming Events</Link>
+          <Link to="/training" className="navbar__link">Training</Link>
+          <Link to="/speakers" className="navbar__link">Speakers</Link>
+          <Link to="/about" className="navbar__link">About</Link>
+        </div>
       </div>
-
-      <div className="navbar__links">
-        <Link to="/" className="navbar__link">Home</Link>
-        <Link to="/events" className="navbar__link">Events</Link>
-        <Link to="/training" className="navbar__link">Training</Link>
-        <Link to="/speakers" className="navbar__link">Speakers</Link>
-        <Link to="/about" className="navbar__link">About</Link>
-      </div>
+      <hr className="navbar__separator" />
     </nav>
   );
 };
