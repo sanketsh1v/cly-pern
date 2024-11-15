1a8076391059bf1b00903f9dcc4f1d8a36e31a92
sh1vrm <sanketh.mekala@edu.sait.ca>
2024-11-14 21:51:56 -0700

    codecov-test-pass
    fixed issues with axios imports in app.test.js

---
+++ client/babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env',    // Transpile modern JavaScript to ES5
+      '@babel/preset-react',  // Transpile JSX syntax for React
+    ],
+  };
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
 
-test('renders main application component', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/app/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('axios');
+
+axios.get.mockResolvedValue({
+  data: { events: [{ name: 'Sample Event', date: '2023-12-12' }] },
+});
+
+test('renders main application component', async () => {
+  await act(async () => {
+    render(<App />);
+  });
+
+  const headingElements = screen.getAllByText(/Calgary Laughter Yoga and Adventures/i);
+  expect(headingElements.length).toBeGreaterThan(0);
 });
